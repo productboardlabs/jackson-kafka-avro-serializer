b6be472f2caa06f99485e6773fe22ace227ec822
Lukáš Křečan <lukas.krecan@productboard.com>
2020-08-06 08:36:45 +0200

    Add missing annotations

--- src/main/java/com/productboard/kafka/serializers/SchemaMetadata.java
+++ src/main/java/com/productboard/kafka/serializers/SchemaMetadata.java
@@ -1,20 +1,24 @@
 package com.productboard.kafka.serializers;
 
 import org.apache.avro.Schema;
+import org.jetbrains.annotations.NotNull;
 
 public class SchemaMetadata {
     private final Schema schema;
     private final String subject;
 
-    public SchemaMetadata(Schema schema, String subject) {
+
+    public SchemaMetadata(@NotNull Schema schema, @NotNull String subject) {
         this.schema = schema;
         this.subject = subject;
     }
 
+    @NotNull
     public Schema getSchema() {
         return schema;
     }
 
+    @NotNull
     public String getSubject() {
         return subject;
     }
