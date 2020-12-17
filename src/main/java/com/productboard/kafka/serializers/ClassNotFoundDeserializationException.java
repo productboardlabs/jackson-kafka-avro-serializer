d4de38957610ccd7492e9befcaa2de914ad87b58
Lukáš Křečan <lukas.krecan@productboard.com>
2020-12-17 15:20:14 +0100

    Do not read schema in exception handling, it may be null

--- src/main/java/com/productboard/kafka/serializers/ClassNotFoundDeserializationException.java
+++ src/main/java/com/productboard/kafka/serializers/ClassNotFoundDeserializationException.java
@@ -6,7 +6,7 @@ import org.apache.kafka.common.errors.SerializationException;
  * Thrown when the class to be deserialized to can not be found.
  */
 public class ClassNotFoundDeserializationException extends SerializationException {
-    public ClassNotFoundDeserializationException(String className, String topic, String schema) {
-        super("Can not get class \"" + className + "\" for topic \"" + topic + "\" and schema \"" + schema + "\"");
+    ClassNotFoundDeserializationException(String className, String topic) {
+        super("Can not get class \"" + className + "\" for topic \"" + topic + "\"");
     }
 }
--- src/main/java/com/productboard/kafka/serializers/DefaultJacksonKafkaAvroDeserializer.java
+++ src/main/java/com/productboard/kafka/serializers/DefaultJacksonKafkaAvroDeserializer.java
@@ -11,7 +11,7 @@ public class DefaultJacksonKafkaAvroDeserializer extends AbstractJacksonKafkaAvr
         try {
             return Class.forName(className);
         } catch (ClassNotFoundException e) {
-            throw new ClassNotFoundDeserializationException(className, topic, schema.getFullName());
+            throw new ClassNotFoundDeserializationException(className, topic);
         }
     }
 
