1a6624116b1bdfd8d2fa5ac8cc2d230365c1601a
Lukas Krecan <lukas@krecan.net>
2021-09-02 19:27:16 +0200

    Autoregister Jackson modules

--- DEPLOYMENT.md
+++ DEPLOYMENT.md
@@ -1,1 +1,7 @@
 Follow https://central.sonatype.org/pages/apache-maven.html
+
+```
+mvn clean deploy
+
+mvn nexus-staging:release
+```
--- src/main/java/io/github/productboardlabs/kafka/serializers/Utils.java
+++ src/main/java/io/github/productboardlabs/kafka/serializers/Utils.java
@@ -3,16 +3,12 @@ package io.github.productboardlabs.kafka.serializers;
 import com.fasterxml.jackson.dataformat.avro.AvroMapper;
 import org.apache.avro.Schema;
 import org.apache.kafka.common.errors.SerializationException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
 
 class Utils {
-    private static final Logger logger = LoggerFactory.getLogger(Utils.class);
-
-    static Schema parseSchema(String path) {
+     static Schema parseSchema(String path) {
         try {
             try (InputStream resource = Thread.currentThread().getContextClassLoader().getResourceAsStream(path)) {
                 if (resource == null) {
@@ -27,11 +23,7 @@ class Utils {
 
     static AvroMapper createAvroMapper() {
         AvroMapper mapper = new AvroMapper();
-        try {
-            mapper.registerModule((com.fasterxml.jackson.databind.Module) Class.forName("com.fasterxml.jackson.module.kotlin.KotlinModule").getDeclaredConstructor().newInstance());
-        } catch (Exception e) {
-            logger.debug("Can not register Kotlin Module", e);
-        }
+        mapper.findAndRegisterModules();
         return mapper;
     }
 }
