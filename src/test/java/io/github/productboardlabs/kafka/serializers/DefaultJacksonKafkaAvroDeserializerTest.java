d54eaf5a56d2ff61dc639fced0e566e6f2c9cbfb
Lukas Krecan <lukas@krecan.net>
2021-10-13 21:03:09 +0200

    Fix tests

--- src/test/java/io/github/productboardlabs/kafka/serializers/DefaultJacksonKafkaAvroDeserializerTest.java
+++ src/test/java/io/github/productboardlabs/kafka/serializers/DefaultJacksonKafkaAvroDeserializerTest.java
@@ -2,10 +2,13 @@ package io.github.productboardlabs.kafka.serializers;
 
 
 import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import org.apache.avro.Schema;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
 import static io.github.productboardlabs.kafka.serializers.TestData.defaultConfig;
 import static io.github.productboardlabs.kafka.serializers.TestData.generatedUser;
+import static io.github.productboardlabs.kafka.serializers.TestData.simpleUser;
 import static io.github.productboardlabs.kafka.serializers.TestData.topic;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,7 +17,12 @@ class DefaultJacksonKafkaAvroDeserializerTest {
     private final KafkaAvroSerializer standardSerializer;
 
     DefaultJacksonKafkaAvroDeserializerTest() {
-        this.deserializer = new DefaultJacksonKafkaAvroDeserializer();
+        this.deserializer = new DefaultJacksonKafkaAvroDeserializer() {
+            @Override
+            protected @NotNull String getClassName(@NotNull String topic, @NotNull Schema schema) {
+                return SimpleUser.class.getCanonicalName();
+            }
+        };
         this.deserializer.configure(defaultConfig(), false);
 
         this.standardSerializer = new KafkaAvroSerializer();
@@ -24,6 +32,6 @@ class DefaultJacksonKafkaAvroDeserializerTest {
     @Test
     void shouldDeserializeObject() {
         byte[] payload = standardSerializer.serialize(topic, generatedUser);
-        assertThat(deserializer.deserialize(topic, payload)).isEqualTo(generatedUser);
+        assertThat(deserializer.deserialize(topic, payload)).isEqualTo(simpleUser);
     }
 }
